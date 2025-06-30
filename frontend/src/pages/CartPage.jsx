bbe2b288c408f37408b747329d06bd53a121d27f
minmin1805 <doanlyminh2005@gmail.com>
2025-06-30 01:54:30 -0400

    finished basic design for cart page

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useCartStore } from "../stores/useCartStore";
 import { FaTrashAlt } from "react-icons/fa";
-
 function CartPage() {
   const { cart, total, coupon, subtotal } = useCartStore();
 
@@ -12,7 +11,7 @@ function CartPage() {
   return (
     <div className="min-h-screen mt-20 flex flex-row gap-5">
       {/* Right Side */}
-      <div className="p-3 ml-20 w-[60%]">
+      <div className="ml-20 w-[60%]">
         {/* Cart Items */}
         <div className="flex flex-col gap-5 w-[100%]">
           {cart.map((eachItem) => (
@@ -38,6 +37,52 @@ function CartPage() {
             </div>
           ))}
         </div>
+
+        <div className="flex flex-col gap-5 mt-10">
+          <h2>People are bought</h2>
+          <div className="flex flex-row gap-5">
+            {/* 3 products - future*/}
+          
+          </div>
+          
+        </div>
+      </div>
+
+      {/* Left Side */}
+      <div className="w-[25%]">
+          <div className="flex flex-col gap-5">
+
+            <div className="flex flex-col gap-2 bg-gray-800 p-5 rounded-lg">
+              <h2>Order Summary</h2>
+              <div className="flex flex-row justify-between items-center">
+                <p>Original Price</p>
+                <p>${total}</p>
+              </div>
+              <div className="w-full h-[1px] bg-gray-700"></div>
+              <div className="flex flex-row justify-between items-center">
+                <p>Total</p>
+                <p>${total}</p>
+              </div>
+              <button className="bg-blue-500 text-white px-3 py-1 rounded-lg">Proceed to Checkout</button>
+              <a href="/" className="text-blue-500 text-center">Continue shopping</a>
+
+            </div>
+
+            <div className="flex flex-col gap-2 bg-gray-800 p-5 rounded-lg">
+              <h2>Do you have a voucher or gift card?</h2>
+              <input type="text" placeholder="Enter voucher code" className="bg-gray-800 text-white px-3 py-1 rounded-lg" />
+              <button className="bg-blue-500 text-white px-3 py-1 rounded-lg">Apply</button>
+              <h2>Your Available Coupon: </h2>
+              {coupon ? (
+                <div className="flex flex-row justify-between items-center">
+                  <p>{coupon.name}</p>
+                </div>
+              ) : (
+                <p>No coupon available </p>
+              )}
+            </div>
+
+          </div>
       </div>
     </div>
   );
