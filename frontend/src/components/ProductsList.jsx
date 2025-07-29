427ebfd46bc73eaeae28a51f628d2e6879fce157
minmin1805 <doanlyminh2005@gmail.com>
2025-07-29 00:19:30 -0400

    fix design for Products page

--- frontend/src/components/ProductsList.jsx
+++ frontend/src/components/ProductsList.jsx
@@ -9,70 +9,77 @@ const ProductsList = () => {
 
 	return (
 		<motion.div
-			className='bg-gray-800 shadow-lg rounded-lg overflow-hidden max-w-4xl mx-auto'
+			className='bg-white shadow-lg rounded-lg overflow-hidden max-w-[85%] mx-auto'
 			initial={{ opacity: 0, y: 20 }}
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ duration: 0.8 }}
 		>
 			<table className=' min-w-full divide-y divide-gray-700'>
-				<thead className='bg-gray-700'>
+				<thead className='bg-white'>
 					<tr>
 						<th
+							style={{ color: "var(--gentle-brown)" }}
 							scope='col'
-							className='px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider'
+							className='px-6 py-3 text-left text-lg font-medium  uppercase tracking-wider'
 						>
 							Product
 						</th>
 						<th
+							style={{ color: "var(--gentle-brown)" }}
 							scope='col'
-							className='px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider'
+							className='px-6 py-3 text-left text-lg font-medium  uppercase tracking-wider'
 						>
 							Price
 						</th>
 						<th
+							style={{ color: "var(--gentle-brown)" }}
 							scope='col'
-							className='px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider'
+							className='px-6 py-3 text-left text-lg font-medium  uppercase tracking-wider'
 						>
 							Category
 						</th>
 
 						<th
+							style={{ color: "var(--gentle-brown)" }}
 							scope='col'
-							className='px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider'
+							className='px-6 py-3 text-left text-lg font-medium  uppercase tracking-wider'
 						>
 							Featured
 						</th>
 						<th
+							style={{ color: "var(--gentle-brown)" }}
 							scope='col'
-							className='px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider'
+							className='px-6 py-3 text-left text-lg font-medium  uppercase tracking-wider'
 						>
 							Actions
 						</th>
 					</tr>
 				</thead>
 
-				<tbody className='bg-gray-800 divide-y divide-gray-700'>
+				<tbody className='bg-white divide-y divide-gray-700'>
 					{products?.map((product) => (
-						<tr key={product._id} className='hover:bg-gray-700'>
+						<tr key={product._id} className='hover:bg-[#FFCCBC]'>
 							<td className='px-6 py-4 whitespace-nowrap'>
 								<div className='flex items-center'>
-									<div className='flex-shrink-0 h-10 w-10'>
+									<div className='flex-shrink-0'>
 										<img
-											className='h-10 w-10 rounded-full object-cover'
+											width={80}
+											height={80}
+											className=' rounded-full object-cover'
 											src={product.image}
 											alt={product.name}
 										/>
 									</div>
 									<div className='ml-4'>
-										<div className='text-sm font-medium text-white'>{product.name}</div>
+										<div className='text-xl font-medium text-[#20090283]'>{product.name}</div>
 									</div>
 								</div>
 							</td>
 							<td className='px-6 py-4 whitespace-nowrap'>
-								<div className='text-sm text-gray-300'>${product.price.toFixed(2)}</div>
+								<div className='text-xl text-[#BF360C]'>${product.price.toFixed(2)}</div>
 							</td>
 							<td className='px-6 py-4 whitespace-nowrap'>
-								<div className='text-sm text-gray-300'>{product.category}</div>
+								<div className='text-xl text-[#BF360C]'>{product.category}</div>
 							</td>
 							<td className='px-6 py-4 whitespace-nowrap'>
 								<button
@@ -84,7 +91,7 @@ const ProductsList = () => {
 									<Star className='h-5 w-5' />
 								</button>
 							</td>
-							<td className='px-6 py-4 whitespace-nowrap text-sm font-medium'>
+							<td className='px-6 py-4 whitespace-nowrap text-xl font-medium'>
 								<button
 									onClick={() => deleteProduct(product._id)}
 									className='text-red-400 hover:text-red-300'
