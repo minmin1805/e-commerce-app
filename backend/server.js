f5f514b39efa98520a7b169e21a352ccba6363e8
minmin1805 <doanlyminh2005@gmail.com>
2025-08-22 21:41:13 -0400

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,14 @@ import cartRoutes from './routes/cart.route.js';
 import couponRoutes from './routes/coupon.route.js';
 import paymentRoutes from './routes/payment.route.js';
 import analyticsRoutes from './routes/analytics.route.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+const __dirname = path.resolve();
 
 app.use(express.json({limit: "20mb"}));
 app.use(cookieParser());
@@ -23,6 +26,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes)
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
